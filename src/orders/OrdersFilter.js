364a5849707c086a98d9de3c9eaa9485fb0c82d0
Guillaume <guillaume.billey@gmail.com>
2021-02-23 15:42:54 +0100

    feat: add status filter

--- src/orders/OrdersFilter.js
+++ src/orders/OrdersFilter.js
@@ -1,7 +1,14 @@
-import { Filter, SearchInput } from "react-admin";
+import { Filter, SearchInput, SelectInput } from "react-admin";
+
+const statusChoices = [
+    { id: "cancelled", name: "Cancelled" },
+    { id: "delivered", name: "Delivered" },
+    { id: "ordered", name: "Ordered" },
+];
 
 export const OrdersFilter = (props) => (
     <Filter {...props}>
         <SearchInput source="q" alwaysOn />
+        <SelectInput source="status" choices={statusChoices} alwaysOn />
     </Filter>
 );
