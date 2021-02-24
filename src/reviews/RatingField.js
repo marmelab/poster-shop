94f4081833a0ba9079fc3c3dfb4def7ec2786e7f
Guillaume <guillaume.billey@gmail.com>
2021-02-24 11:12:45 +0100

    chore: apply Array.from(Array.keys()) trick

--- src/reviews/RatingField.js
+++ src/reviews/RatingField.js
@@ -1,20 +1,11 @@
-import PropTypes from "prop-types";
 import * as React from "react";
 
 export const RatingField = ({ source, record = {} }) => {
-    const displayStars = () => {
-        const stars = [];
-        for (let index = 0; index < record[source]; index++) {
-            stars.push(<span>*</span>);
-        }
-        return stars;
-    };
-
-    return <span>{displayStars()}</span>;
-};
-
-RatingField.propTypes = {
-    label: PropTypes.string,
-    record: PropTypes.object,
-    source: PropTypes.string.isRequired,
+    return (
+        <span>
+            {Array.from(Array(record[source]).keys()).map(() => (
+                <span>*</span>
+            ))}
+        </span>
+    );
 };
