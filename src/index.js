a89d80553824f6ac33074c6b0c1303605549c4c9
Guillaume <guillaume.billey@gmail.com>
2021-03-04 10:04:27 +0100

    chore: use CssBaseLine

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+import CssBaseline from "@material-ui/core/CssBaseline";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-document.body.style.margin = "0px";
 ReactDOM.render(
     <React.StrictMode>
+        <CssBaseline />
         <App />
     </React.StrictMode>,
     document.getElementById("root")
