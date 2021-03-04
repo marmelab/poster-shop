673196dc0b74456eda396c556a26ce875dd5a7a1
Guillaume <guillaume.billey@gmail.com>
2021-03-04 10:18:20 +0100

    fix: check record exists

--- src/orders/OrderItems.js
+++ src/orders/OrderItems.js
@@ -18,85 +18,92 @@ import {
 export const OrderItems = ({ record }) => {
     return (
         <TableContainer component={Paper}>
-            <Table aria-label="items">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Reference</TableCell>
-                        <TableCell align="right">Unit price</TableCell>
-                        <TableCell align="right">Quantity</TableCell>
-                        <TableCell align="right">Total</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {record.basket
-                        ? record.basket.map((item) => (
-                              <TableRow key={item.product_id}>
-                                  <TableCell>
-                                      <ReferenceField
-                                          source="product_id"
-                                          reference="products"
-                                          resource="commands"
-                                          record={item}
-                                          link="show"
-                                          basePath="/commands"
-                                      >
-                                          <TextField source="reference" />
-                                      </ReferenceField>
-                                  </TableCell>
-                                  <TableCell align="right">
-                                      <ReferenceField
-                                          source="product_id"
-                                          reference="products"
-                                          resource="commands"
-                                          record={item}
-                                          link={false}
-                                          basePath="/commands"
-                                      >
-                                          <NumberField
-                                              source="price"
-                                              options={{
-                                                  style: "currency",
-                                                  currency: "USD",
+            {record && (
+                <Table aria-label="items">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Reference</TableCell>
+                            <TableCell align="right">Unit price</TableCell>
+                            <TableCell align="right">Quantity</TableCell>
+                            <TableCell align="right">Total</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {record.basket
+                            ? record.basket.map((item) => (
+                                  <TableRow key={item.product_id}>
+                                      <TableCell>
+                                          <ReferenceField
+                                              source="product_id"
+                                              reference="products"
+                                              resource="commands"
+                                              record={item}
+                                              link="show"
+                                              basePath="/commands"
+                                          >
+                                              <TextField source="reference" />
+                                          </ReferenceField>
+                                      </TableCell>
+                                      <TableCell align="right">
+                                          <ReferenceField
+                                              source="product_id"
+                                              reference="products"
+                                              resource="commands"
+                                              record={item}
+                                              link={false}
+                                              basePath="/commands"
+                                          >
+                                              <NumberField
+                                                  source="price"
+                                                  options={{
+                                                      style: "currency",
+                                                      currency: "USD",
+                                                  }}
+                                              />
+                                          </ReferenceField>
+                                      </TableCell>
+                                      <TableCell align="right">
+                                          {item.quantity}
+                                      </TableCell>
+                                      <TableCell align="right">
+                                          <ReferenceFieldController
+                                              source="product_id"
+                                              reference="products"
+                                              record={item}
+                                              basePath="/commands"
+                                          >
+                                              {({
+                                                  referenceRecord,
+                                                  ...props
+                                              }) => {
+                                                  return (
+                                                      <span>
+                                                          <NumberField
+                                                              record={{
+                                                                  total: referenceRecord
+                                                                      ? referenceRecord.price *
+                                                                        item.quantity
+                                                                      : "-",
+                                                              }}
+                                                              source="total"
+                                                              options={{
+                                                                  style:
+                                                                      "currency",
+                                                                  currency:
+                                                                      "USD",
+                                                              }}
+                                                          />
+                                                      </span>
+                                                  );
                                               }}
-                                          />
-                                      </ReferenceField>
-                                  </TableCell>
-                                  <TableCell align="right">
-                                      {item.quantity}
-                                  </TableCell>
-                                  <TableCell align="right">
-                                      <ReferenceFieldController
-                                          source="product_id"
-                                          reference="products"
-                                          record={item}
-                                          basePath="/commands"
-                                      >
-                                          {({ referenceRecord, ...props }) => {
-                                              return (
-                                                  <span>
-                                                      <NumberField
-                                                          record={{
-                                                              total: referenceRecord
-                                                                  ? referenceRecord.price *
-                                                                    item.quantity
-                                                                  : "-",
-                                                          }}
-                                                          source="total"
-                                                          options={{
-                                                              style: "currency",
-                                                              currency: "USD",
-                                                          }}
-                                                      />
-                                                  </span>
-                                              );
-                                          }}
-                                      </ReferenceFieldController>
-                                  </TableCell>
-                              </TableRow>
-                          ))
-                        : null}
-                </TableBody>
-            </Table>
+                                          </ReferenceFieldController>
+                                      </TableCell>
+                                  </TableRow>
+                              ))
+                            : null}
+                    </TableBody>
+                </Table>
+            )}
         </TableContainer>
     );
 };
